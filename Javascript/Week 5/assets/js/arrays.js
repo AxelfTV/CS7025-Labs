127e13691edc6aa439c479336467c449fa25912a
Axelf <axelf196@gmail.com>
2024-10-10 13:01:48 +0100

    added kanye api

--- Javascript/Week 5/assets/js/arrays.js
+++ Javascript/Week 5/assets/js/arrays.js
@@ -4,29 +4,31 @@ let color1 = Math.floor(Math.random()*16777215);
 let color2 = Math.floor(Math.random()*16777215);
 let button;
 let image;
+let quoteText;
 function main(){
     body = document.body;
     button = document.getElementById("colors");
-    button.addEventListener("click", OnClickColor);
+    button.addEventListener("click", onClickColor);
     image = document.createElement("img");
+    quoteText = document.getElementById("kanye");
     document.body.append(image);
 
+    updateElements();
+    
     setInterval(tick, 100);
 }
 
 function tick(){
     let d = new Date();
-    /*body.style.backgroundColor = "#" + Math.floor(Math.random()*16777215).toString(16);
     
-    let colorInterp = (Math.sin(d.getTime()/500000)+1)/2;
-    let finalColor = Math.round((colorInterp*color1 + (1 - colorInterp) * color2)).toString(16);
-    console.log("#" + finalColor)
-    //body.style.backgroundColor = "#" + finalColor;
-    */
     hue = Math.round(((d.getTime()/15)%360)).toString();
     body.style.backgroundColor = 'hsl(' + hue + ' 45% 60%)';
+  
 }
-function OnClickColor(){
+function onClickColor(){
+    updateElements();
+}
+function updateElements(){
     let apiCall = "https://dog.ceo/api/breeds/image/random"
     fetch(apiCall)
         .then(response => {
@@ -34,5 +36,15 @@ function OnClickColor(){
         })
         .then(data =>{
             image.src = data.message;
+
         })
+        let apiCall2 = "https://api.kanye.rest/";
+        fetch(apiCall2)
+            .then(response => {
+                return response.json();
+            })
+            .then(data =>{
+                quoteText.textContent = data.quote;
+    
+            })
 }
